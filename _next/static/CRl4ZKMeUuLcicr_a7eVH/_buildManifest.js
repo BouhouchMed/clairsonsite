5df597591f9799b5f192cb9cc05aaf27585f5dea
bouhouchmed <medbouhouch1@gmail.com>
2026-08-29 02:04:22 +0100

    Deploy Clairson static site

---
+++ _next/static/CRl4ZKMeUuLcicr_a7eVH/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/clairsonsite//_next/:path+",
+        "destination": "/clairsonsite/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
